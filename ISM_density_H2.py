8bdce9e3972d7d17bf32a5ae8cc2d557ee4e1684
Moana <lioni-moana.bourguinat@gssi.it>
2024-04-08 16:12:47 +0200

    modified:   ISM_density_H2.py

--- ISM_density_H2.py
+++ ISM_density_H2.py
@@ -2,6 +2,7 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+import scipy.stats as stats
 from zeroordergalaxy import give_n_ISM
 from sn_bubble import make_lognormal
 
